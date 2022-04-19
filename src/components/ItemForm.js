ccbb01e13b0d165bc7854df8d9d173b73330898e
Kevin Rogers <kevinarogers1@gmail.com>
2022-04-18 21:44:19 -0400

    Completed assignment

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+
+function ItemForm({onItemFormSubmit}) {
+  const [formData, formDataSetter] = useState({
+    name: "", 
+    category:"Produce",
+  })
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    
+    const newItem = {
+      id: uuid(),
+      ...formData
+    }; 
+
+    console.log(newItem);
+    onItemFormSubmit(newItem);
+  }
+
+  function handleChange(event) {
+    formDataSetter({...formData, 
+      [event.target.name]: event.target.value
+    })
+
+  }
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit} >
       <label>
         Name:
-        <input type="text" name="name" />
+        <input 
+          type="text" 
+          name="name" 
+          value={formData.name}
+          onChange={handleChange}
+        />
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select 
+        name="category"
+        value={formData.category}
+        onChange={handleChange}
+        >
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
